006dbefc7d9427ed755e7aa8c429234cb9b88c6d
Laura <laurabande10@gmail.com>
2020-12-10 19:48:41 +0100

    Añadidos comentarios 2

--- src/main/java/racionalLauraDani/AppRacional.java
+++ src/main/java/racionalLauraDani/AppRacional.java
@@ -14,21 +14,33 @@ import java.text.DecimalFormat;
 public class AppRacional {
 
     public static void main(String[] args) {
+    	
+    	//Declaración de objetos
         Racional z = new Racional(4, 5);
         Racional k = new Racional(3, 4);
 
+        //Creación de objeto random
         Racional random = new Racional();
 
         random = random.aleatorio();
 
+        //Impresión de valores de objetos
         z.imprimirConsola();
         k.imprimirConsola();
         random.imprimirConsola();
 
+        //Comparación de racionales z y x
         System.out.println("¿Son los valores iguales? " + Racional.igualdad(z, k));
-
-        System.out.print("La suma de z y k es: ");
+        
+        //Suma de z y k
         z.suma(k);
+
+        System.out.println("La suma de z y k es: " + z.toString());
+        
+        //División de los valores actualizados de z y k
+        Racional div = z.division(k);
+        
+        System.out.println("El resultado de la división es: " + div.toString());
 
     }
 }

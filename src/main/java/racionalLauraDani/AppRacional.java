4266e2fdf2d250e0107f4dd89efef472c8bccabc
daniel <danisimarroreigada@gmail.com>
2020-12-10 14:40:17 +0100

    Creacion racional aleatori y imprimir los tres relacionales

--- src/main/java/racionalLauraDani/AppRacional.java
+++ src/main/java/racionalLauraDani/AppRacional.java
@@ -5,6 +5,8 @@
  */
 package racionalLauraDani;
 
+import java.text.DecimalFormat;
+
 /**
  *
  * @author daniel
@@ -13,5 +15,14 @@ public class AppRacional {
     public static void main(String[] args) {
         Racional z = new Racional (4, 5);
         Racional k = new Racional (3, 4);
+        
+        Racional random = new Racional ();
+        
+        random=random.aleatorio();
+        
+        DecimalFormat df = new DecimalFormat("#.00");
+        
+        System.out.println(df.format( z + " - " + k + " - " + random  ));
+    
     }
 }

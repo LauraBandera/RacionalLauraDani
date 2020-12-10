c2c7e31d8faaab0a0b0fbdaedaf21c669cb1f109
daniel <danisimarroreigada@gmail.com>
2020-12-10 13:04:03 +0100

    Creacion de constructo parametrizado y metodos get

--- src/main/java/racionalLauraDani/Racional.java
+++ src/main/java/racionalLauraDani/Racional.java
@@ -10,12 +10,31 @@ package racionalLauraDani;
  * @author laura
  */
 public class Racional {
+
     private int a /*numerador*/, b /*denominado*/;
 
     public Racional() {
         this.a = 1;
         this.b = 1;
     }
-    
-    
+
+    public Racional(int a, int b) {
+        this.a = a;
+
+        if (b == 0) {
+            this.b = 1;
+        }
+        
+     
+
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
 }
